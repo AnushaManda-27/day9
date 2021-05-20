bc91cc11ce5c8e7fea444bfa8907924f0d2bb8bf
AnushaManda-27 <anushamanda27@gmail.com>
2021-05-20 16:05:29 +0530

    Added: Pincode with 6 digits

--- PinCode.java
+++ PinCode.java
@@ -1,10 +1,25 @@
-
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class PinCode {
-	
-	public static void main(String[] args) {
-	
-		System.out.println("WElcome");
-	}
+    public static boolean isValid(String pincode) {
+
+      String ZIP = "[1-9]{1}[0-9]{5}";
+
+      Pattern p = Pattern.compile(ZIP);
+
+      if (pincode == null){
+         return false;
+      }
+
+      Matcher m = p.matcher(pincode);
+      return m.matches();
+      }
+
+   public static void main(String args[]){
+
+      String Pincode1 ="400708";
+      System.out.println( Pincode1 +":" +isValid(Pincode1));
+   }
 
 }
